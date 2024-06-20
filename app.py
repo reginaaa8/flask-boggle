50e15392f136fa80b786fb45c54afbb296eec2b0
Regina Steffen <reginasteffen@live.com>
2024-06-20 13:54:26 -0500

    displayed boggle board; added form for user to submit guess (need to add action to form)

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from boggle import Boggle
-from flask import Flask
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
 
 boggle_game = Boggle()
+
+@app.route('/')
+def show_board():
+    '''show boggle board'''
+    board = boggle_game.make_board()
+    session['board'] = board
+
+    return render_template('board.html', board=board)

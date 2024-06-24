1b183ab329985c7b376062d33cac52b623d3bc4f
Regina Steffen <reginasteffen@live.com>
2024-06-23 20:49:54 -0500

    show user a message based on response received from server

--- static/boggle.js
+++ static/boggle.js
@@ -7,13 +7,41 @@ $guessForm.on('submit', async function(e){
     e.preventDefault();
     
     // send word to server to see if it's a valid word in the dictionary / on board
-    let userGuess = $('#user-guess').val();
+    const userGuess = $('#user-guess').val();
+    
+    // if user submits form without entering, any input, display message and return
+    // show message to user based on response received from server 
+    let $msg = $('.msg');
+    if (userGuess == ''){
+        $msg.replaceWith ('<p id ="empty-msg" class="msg"> Invalid. Please enter a word. </p>');
+        $msg = $('.msg');
+        return;
+    }
 
-    response = await axios({
+    const response = await axios({
         method: 'post',
         url: '/check-word',
         data:{userGuess: userGuess}
     });
-    
+
+    result = response.data['result'];
+
+    if(result == 'ok'){
+        $msg.replaceWith ('<p id="ok-msg" class ="msg"> Great! </p>');
+        $msg = $('.msg');
+    }
+    if(result == 'not-word'){
+        $msg.replaceWith ('<p id="not-word-msg" class ="msg">That is not a valid word. PLease try again.</p>');
+        $msg = $('.msg');
+    }
+    if(result == 'not-on-board'){
+        $msg.replaceWith ('<p id="not-on-board-msg" class ="msg">That word is not on the board. Please try again.</p>');
+        $msg = $('.msg');
+    }
+
+
+    // clear input text box after form submitted
+    $('#user-guess').val('');
+
 });
 
